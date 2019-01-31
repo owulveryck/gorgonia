8ef41bb9d7b85d8c139d6c88e00cbccb6860f94c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-01-31 22:45:46 +0100

    WIP

--- ops/utils.go
+++ ops/utils.go
@@ -1,11 +1,15 @@
 package ops
 
-import "github.com/pkg/errors"
+import "log"
 
 // CheckArity returns an error if the input number does not correspond to the expected arity
 func CheckArity(op Arityer, inputs int) error {
 	if inputs != op.Arity() && op.Arity() >= 0 {
-		return errors.Errorf("%v has an arity of %d. Got %d instead", op, op.Arity(), inputs)
+		// TODO: deal with that
+		//return errors.Errorf("%v has an arity of %d. Got %d instead", op, op.Arity(), inputs)
+		log.Printf("%v has an arity of %d. Got %d instead", op, op.Arity(), inputs)
+
+		return nil
 	}
 	return nil
 }
